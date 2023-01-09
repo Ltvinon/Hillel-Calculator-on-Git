1bcdedd00c1edc8c18b420991d9038cd5c960446
Ltvinon <ltvinon@gmail.com>
2023-01-09 13:48:28 +0200

    Creating project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hillel_Calculator_on_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

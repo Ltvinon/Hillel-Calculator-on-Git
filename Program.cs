8f82e92ed52ac19db286ef54ad16edef3addded3
Ltvinon <ltvinon@gmail.com>
2023-01-09 15:08:11 +0200

    Make a mathod for conversion (ToPoland)

--- Program.cs
+++ Program.cs
@@ -6,6 +6,39 @@
         {
             
         }
+        static private string ToPoland(string input)
+        {
+            string output = string.Empty;
+            Stack<char> opers = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (IsDelim(input[i]))
+                {
+                    continue;
+                }
+                else if (Char.IsDigit(input[i]))
+                {
+                    while (!IsDelim(input[i]) && !IsOper(input[i]))
+                    {
+                        output += input[i];
+                        i++;
+
+                        if (i == input.Length)
+                        {
+                            break;
+                        }
+                    }
+                    output += " ";
+                    i--;
+                }
+            }
+            while (opers.Count > 0)
+            {
+                output += opers.Pop() + " ";
+            }
+            return output;
+        }
         static private bool IsDelim(char c)
         {
             if (" =".IndexOf(c) != -1)
